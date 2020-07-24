99f8058cf86021f0482cf0c43b60a986ac6f3ea7
RushiVivek <51112533+RushiVivek@users.noreply.github.com>
2020-07-24 12:56:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function f1() {
+    document.getElementById('title').innerHTML = "Surprise";
+    document.body.innerHTML = "<div class='section' id='header' class='section'><p>Mom Happy Birthday<p></div>";
+    document.body.innerHTML += "<div class='section' id='img' class='section'><video controls autoplay loop width='600'><source src='mom.mp4' type='audio/mp4'>Video is not supported by your browser</video></div>";
+}
